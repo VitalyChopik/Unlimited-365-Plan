101c7fb86adba654b5b565fc2e698070364bddc8
VitalyChopik <leo.space97@gmail.com>
2023-10-25 19:12:26 +0300

    feat: сделаны блоки

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,6 @@
-/**
- * !(i)
- * Код попадает в итоговый файл, только когда вызвана функция, например FLSFunctions.spollers();
- * Или когда импортирован весь файл, например import "files/script.js";
- * Неиспользуемый код в итоговый файл не попадает.
 
- * Если мы хотим добавить модуль следует его раскомментировать
- */
-// import MousePRLX from './libs/parallaxMouse'
-// import AOS from 'aos'
-// import Swiper, { Navigation, Pagination } from 'swiper';
-
+import AOS from 'aos'
 import BaseHelpers from './helpers/BaseHelpers.js';
-import PopupManager from './modules/PopupManager';
-import BurgerMenu from './modules/BurgerMenu';
-import Tabs from './modules/Tabs';
-import Accordion from './modules/Accordion.js';
 
 BaseHelpers.checkWebpSupport();
 
@@ -22,43 +8,36 @@ BaseHelpers.addTouchClass();
 
 BaseHelpers.addLoadedClass();
 
-// BaseHelpers.headerFixed();
+AOS.init();
 
-/**
- * Открытие/закрытие модальных окон
- * Чтобы модальное окно открывалось и закрывалось
- * На окно повешай атрибут data-popup="<название окна>"
- * На кнопку, которая вызывает окно повешай атрибут data-type="<название окна>"
+const faqBoxs = document.querySelectorAll('.faq__box');
 
- * На обертку(.popup) окна добавь атрибут '[data-close-overlay]'
- * На кнопку для закрытия окна добавь класс '.button-close'
- * */
-// new PopupManager();
 
-/**
- *  Модуль для работы с меню (Бургер)
- * */
-new BurgerMenu().init();
+if (faqBoxs[0]) {
+  faqBoxs.forEach((faqBox) => {
+    const question = faqBox.querySelector('.faq__box-question');
+    question.addEventListener('click', () => {
+      faqBox.classList.toggle('active');
+    })
+  });
+  faqBoxs[0].click();
+}
 
-/**
- *  Библиотека для анимаций
- *  документация: https://michalsnik.github.io/aos
- * */
-// AOS.init();
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+anchorLinks.forEach(function (anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
 
-/**
- * Параллакс мышей
- * */
-// new MousePRLX();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
 
-// new Tabs('tabs-example', {
-// 	onChange: (data) => {
-// 		console.log(data);
-// 	},
-// });
+    if (targetElement) {
+      const targetPosition = targetElement.offsetTop;
 
-// new Accordion('.accordion', {
-// 	shouldOpenAll: false, // true
-// 	defaultOpen: [], // [0,1]
-// 	collapsedClass: 'open',
-// });
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
